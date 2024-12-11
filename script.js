70c88542d579dc28e847f73902ee67ad8bd52d16
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2024-12-11 10:06:43 +0100

    Pig game finish

---
+++ script.js
@@ -0,0 +1,95 @@
+"use strict";
+
+const playerScore1 = document.querySelector(".playerScore1");
+const currentScore1 = document.querySelector(".currentScore1");
+const playerScore2 = document.querySelector(".playerScore2");
+const currentScore2 = document.querySelector(".currentScore2");
+const newGame = document.querySelector(".newGame");
+const rollDice = document.querySelector(".rollDice");
+const hold = document.querySelector(".hold");
+const rollNumber = document.querySelector(".rollNumber");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+
+function pigGameCreator() {
+  let player1Score = 0;
+  let player2Score = 0;
+  let player1CurrentScore = 0;
+  let player2CurrentScore = 0;
+  let randomNumber = Math.ceil(Math.random() * 6);
+  let activePlayer = "player1";
+
+  const updateScores = () => {
+    playerScore1.textContent = player1Score;
+    currentScore1.textContent = player1CurrentScore;
+    playerScore2.textContent = player2Score;
+    currentScore2.textContent = player2CurrentScore;
+    rollNumber.textContent = randomNumber;
+  };
+
+  const switchPlayer = () => {
+    activePlayer = activePlayer === "player1" ? "player2" : "player1";
+  };
+
+  const updateUI = (activeSide, inactiveSide, scoreElement, newScore) => {
+    scoreElement.textContent = newScore;
+    activeSide.style.backgroundColor = "rgb(248, 211, 218)";
+    inactiveSide.style.backgroundColor = "rgb(214, 151, 162)";
+  };
+
+  const rollHandler = () => {
+    randomNumber = Math.ceil(Math.random() * 6);
+    rollNumber.textContent = randomNumber;
+
+    if (randomNumber === 1) {
+      switchPlayer();
+    } else {
+      if (activePlayer === "player1") {
+        player1CurrentScore += randomNumber;
+        player2CurrentScore = 0;
+        updateUI(left, right, currentScore1, player1CurrentScore);
+      } else {
+        player2CurrentScore += randomNumber;
+        player1CurrentScore = 0;
+        updateUI(right, left, currentScore2, player2CurrentScore);
+      }
+    }
+    updateScores();
+  };
+
+  const holdHandler = () => {
+    if (activePlayer === "player1") {
+      player1Score += player1CurrentScore;
+      player1CurrentScore = 0;
+      updateUI(right, left, playerScore1, player1Score);
+      switchPlayer();
+    } else {
+      player2Score += player2CurrentScore;
+      player2CurrentScore = 0;
+      updateUI(left, right, playerScore2, player2Score);
+      switchPlayer();
+    }
+    checkWinner();
+    updateScores();
+  };
+
+  const checkWinner = () => {
+    if (player1Score >= 20) {
+      playerScore1.textContent = "Winner!";
+      left.style.backgroundColor = "black";
+    } else if (player2Score >= 20) {
+      playerScore2.textContent = "Winner!";
+      right.style.backgroundColor = "black";
+    }
+  };
+
+  return {
+    rollDice: rollHandler,
+    holdScore: holdHandler,
+  };
+}
+
+const pigGame = pigGameCreator();
+
+rollDice.addEventListener("click", pigGame.rollDice);
+hold.addEventListener("click", pigGame.holdScore);
